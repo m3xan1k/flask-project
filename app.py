8a301f33b308be67f97e075c6b42ffd2d2e2114e
Shevtsov Sergey <job.shevtsov@ya.ru>
2019-06-21 13:44:24 +0300

    authors search

--- app.py
+++ app.py
@@ -222,6 +222,13 @@ def author_detail(id):
 def about():
     return render_template('about.html')
     
-    
+
+@app.route('/search')
+def search():
+    q = request.args.get('q')
+    authors = Author.query.filter(Author.name.like('%' + q + '%')).all()
+    return render_template('index.html', authors=authors)
+
+
 if __name__ == '__main__':
     app.run(debug=True)
